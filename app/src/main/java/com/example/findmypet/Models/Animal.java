108c001a1b81f223423fbcd046651f1d69efddad
Natanael <natanso.s17@hotmail.com>
2019-02-03 17:21:04 -0300

    Criação da tela de abertura

---
+++ app/src/main/java/com/example/findmypet/Models/Animal.java
@@ -0,0 +1,8 @@
+package com.example.findmypet.Models;
+
+public class Animal {
+    private String especie;
+    private String cor;
+    private String raca;
+
+}
---
+++ app/src/main/java/com/example/findmypet/TelaAbertura.java
@@ -0,0 +1,29 @@
+package com.example.findmypet;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaAbertura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_abertura);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarHome();
+            }
+        }, 2000);
+    }
+
+    private void mostrarHome() {
+        Intent intent = new Intent(this,
+                MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
