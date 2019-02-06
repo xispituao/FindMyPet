6c4fdd1e6bf8cd2839a2ea8767290a3deafea6fc
Natanael <natanso.s17@hotmail.com>
2019-02-06 00:49:24 -0300

    Criação do Nav Bar

--- app/src/main/java/com/example/findmypet/MainActivity.java
+++ app/src/main/java/com/example/findmypet/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.findmypet;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionBarContainer;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.findmypet.Adapters.AdapterAnimalPerdido;
 import com.example.findmypet.Models.Animal;
@@ -12,7 +19,9 @@ import com.example.findmypet.Models.Animal;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerview;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     Animal[] animais = {new Animal("rex","Cachorro", "Preta", "Labrador","Perdi ontem a noite"),
             new Animal("garfield","Gato", "Preta", "Siames","Sumiu simplesmente"),new Animal("Pitu","Gato", "Branca", "Siames","Nao seiiii")
     ,new Animal("Bob","Gato", "Preta", "Seila","correu atras de um inseto e desapareceu")};
@@ -22,7 +31,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
+        drawerLayout = (DrawerLayout) findViewById(R.id.dl);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.Open, R.string.Close );
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if(id == R.id.teste){
+                    Toast.makeText(MainActivity.this, "TESTE!",Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.teste2){
+                    Toast.makeText(MainActivity.this, "TESTE2!",Toast.LENGTH_SHORT).show();
+                } else if(id == R.id.teste3){
+                    Toast.makeText(MainActivity.this, "TESTE3!",Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+        RecyclerView recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
         recyclerview.setHasFixedSize(true);
 
         LinearLayoutManager linearlayout = new LinearLayoutManager(this);
@@ -30,5 +64,10 @@ public class MainActivity extends AppCompatActivity {
         AdapterAnimalPerdido adapter = new AdapterAnimalPerdido(this, animais);
         recyclerview.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
